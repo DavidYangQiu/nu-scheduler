d55baad07f9c4db57470b18f4cbf64b224fcddbd
git_DavidQiuMAC <yangqiu2024@u.northwestern.edu>
2022-09-28 16:42:01 -0500

    Show a list of courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,61 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner';'./components/Banner';
+import CourseList from './components/CourseList';'./components/CourseList'
 
 // Display Current Time 
-const App = () => {
-  const today = new Date();
-  const day = today.toLocaleString([], {weekday: 'long'});
-  const date = today.toLocaleDateString([], {dateStyle: 'long'})
+// const App = () => {
+//   const today = new Date();
+//   const day = today.toLocaleString([], {weekday: 'long'});
+//   const date = today.toLocaleDateString([], {dateStyle: 'long'})
 
-  return (
-    <div>
-      <h1>Sample React code</h1>
-      Dispaly Current Time
-      <p>Today is {day}, {date}.</p>
-    </div>
-  );
+//   return (
+//     <div>
+//       <h1>Sample React code</h1>
+//       Dispaly Current Thit ime
+//       <p>Today is {day}, {date}.</p>
+//     </div>
+//   );
+// };
+
+
+const schedule = {
+  "title": "CS Courses for 2018-2019",
+  "courses": {
+    "F101" : {
+      "term": "Fall",
+      "number": "101",
+      "meets" : "MWF 11:00-11:50",
+      "title" : "Computer Science: Concepts, Philosophy, and Connections"
+    },
+    "F110" : {
+      "term": "Fall",
+      "number": "110",
+      "meets" : "MWF 10:00-10:50",
+      "title" : "Intro Programming for non-majors"
+    },
+    "S313" : {
+      "term": "Spring",
+      "number": "313",
+      "meets" : "TuTh 15:30-16:50",
+      "title" : "Tangible Interaction Design and Learning"
+    },
+    "S314" : {
+      "term": "Spring",
+      "number": "314",
+      "meets" : "TuTh 9:30-10:50",
+      "title" : "Tech & Human Interaction"
+    }
+  }
 };
 
+const App = () => (
+  <div>
+    <h1>{schedule.title}</h1>
+    <Banner text={schedule.title}/> 
+    <CourseList courses={schedule.courses}/>
+  </div>
+);
+
 export default App;
-
-
---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+const Banner = ({title}) => {
+  return (
+        <h1>{title}</h1>
+
+  );
+};
+  
+
+   
+
+
+export default Banner;
+
+
